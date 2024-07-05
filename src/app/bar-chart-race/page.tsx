974f8ee426218ee1fe26663a89c17a861d936312
huuquyetng <huuquyetng@gmail.com>
2024-07-05 03:38:07 +0000

    feat: add bar chart race demo

---
+++ src/app/bar-chart-race/page.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import notebook from '@d3/bar-chart-race'
+import { Inspector, Runtime } from '@observablehq/runtime'
+import Link from 'next/link'
+import { useEffect, useRef } from 'react'
+
+export default function BarChartRace() {
+  const ref = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const runtime = new Runtime()
+    runtime.module(notebook, (name: string) => {
+      if (name === 'chart') {
+        return new Inspector(ref.current)
+      }
+    })
+    return () => runtime.dispose()
+  }, [])
+
+  return (
+    <>
+      <h1>
+        Hello, Observable! <Link href="/">Back to home &lt;-</Link>
+      </h1>
+
+      <div ref={ref} />
+    </>
+  )
+}
